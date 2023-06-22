08c31d2ef6c6ff32718982829c9c9690df987c4d
ArthurNavasardyan <arthur.navasardyan5@gmail.com>
2023-06-22 12:59:03 +0400

    First case in C#

---
+++ MathCalculation1/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace MathCalculation1
+{
+     class Calculator
+    {
+        
+        public double MathAction()
+        {
+            double rezult = 0;
+
+            try
+            {
+                Console.WriteLine("Write the first number");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(" select the action + , - , * , / ");
+                string c = Console.ReadLine();
+                Console.WriteLine("Write the second number");
+                double b = Convert.ToDouble(Console.ReadLine());
+                if (b == 0)
+                {
+                    Console.WriteLine(" A number is not divisible by zero ");
+                    return 0;
+                }
+                switch (c)
+                {
+                    case "+":
+                        rezult = a + b;
+                        break;
+                    case "-":
+                        rezult = a - b;
+                        break;
+                    case "*":
+                        rezult = a * b;
+                        break;
+                    case "/":
+                        rezult = a / b;
+                        break;
+                    default:
+                        Console.WriteLine("There is no such action");
+                        break;
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" type could not be changed ");
+            }
+                                             
+            return rezult;
+        }
+                   
+     }
+}
---
+++ MathCalculation1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCalculation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.MathAction());
+        }
+    }
+}
